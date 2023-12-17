8ff89371fb73d6aae60209dd06407fff4edef69e
rizalhasbianto <rizalhasbianto@gmail.com>
2023-12-17 23:42:41 +0700

    change login img

--- src/layouts/auth/layout.js
+++ src/layouts/auth/layout.js
@@ -97,7 +97,7 @@ export const Layout = (props) => {
             </Typography>
             <img
               alt=""
-              src="/assets/auth-illustration.svg"
+              src="/assets/auth-illustration.webp"
             />
           </Box>
         </Grid>
