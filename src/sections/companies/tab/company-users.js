f6d3177156fe68a442d500c1f39823ffdf322295
rizalhasbianto <rizalhasbianto@gmail.com>
2024-01-01 10:40:39 +0700

    Update company-users.js

--- src/sections/companies/tab/company-users.js
+++ src/sections/companies/tab/company-users.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-max-props-per-line */
 import { useCallback, useState } from "react";
 import {
   Box,
@@ -186,7 +187,12 @@ export const CompanyUsers = (props) => {
 
   return (
     <Box sx={{ m: -1.5 }}>
-      <Grid container spacing={1} alignItems={"center"} justifyItems={"flex-start"}>
+      <Grid 
+      container 
+      spacing={1} 
+      alignItems={"center"} 
+      justifyItems={"flex-start"}
+    >
         <Grid item xs={4} md={3}>
           <Typography variant="subtitle2" color="neutral.500">
             Default Contact
