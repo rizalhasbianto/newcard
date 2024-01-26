5f183a3669d1245b4fdbd17b105513a832eaa48a
rizalhasbianto <rizalhasbianto@gmail.com>
2024-01-26 18:26:39 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,6 @@ import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
 export default withAuth(
-  function middleware(req) {
-    if ( req.nextUrl.pathname.startsWith('/companies') && req.nextauth.token.role === "customer" ) {
-        return NextResponse.redirect(new URL('/', req.url));
-    }
-  },
   {
     callbacks: {
       authorized({ token }) {
